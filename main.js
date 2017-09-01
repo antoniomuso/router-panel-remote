2abbb127a1769742e6ccae569976c0e510e9740f
AntonioMusolino <antoniomusolino007@gmail.com>
2017-09-01 12:05:48 +0200

    add server port

--- main.js
+++ main.js
@@ -5,15 +5,19 @@ const optionDefinitions = [
     { name:'serverMode',type: Boolean, defaultValue: false},
     { name: 'router', alias: 'r', type: String , defaultValue: 'http://192.168.1.1' },
     { name: 'server', alias: 's',type: String },
-    { name: 'clientPort', alias: 'p', type:Number, defaultValue: 4400 }
+    { name: 'clientPort', alias: 'p', type:Number, defaultValue: 4400 },
+    { name: 'serverPort', type: Number }
   ]
 
 const options = commandLine(optionDefinitions)
 
 
 if (options.serverMode) {
-    proxy(options.router, 5656)
+    if (!options.serverPort) throw new Error('--serverPort must be specifie')
+    proxy(options.router, options.serverPort)
+    console.log('server mode active')
 } else {
+    if (!options.serverPort) throw new Error('--serverPort must be specifie')
     if (!options.server) throw new Error('In not server mode the ip of server must be specifie')
-    proxy(options.server +':5656' ,options.clientPort)
+    proxy(options.server +`:${options.serverPort}` ,options.clientPort)
 }
